7b9287b03d6493b52fa0bb5c14a5bf6a1738080c
Emmanuel Asante <emmanuel.asante@example.com>
2024-10-02 13:45:00 -1200

    Add build files to repository

---
+++ dist/hotel/350.5519eb5e59c3fc44.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkHotel=self.webpackChunkHotel||[]).push([[350],{8350:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
