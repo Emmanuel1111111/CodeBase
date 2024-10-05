f39626907054deee5f7e2a84d9984c7a4c4950b1
Emmanuel Asante <emmanuel.asante@example.com>
2024-10-05 08:37:59 -1200

    configuration added

--- dist/hotel/350.5519eb5e59c3fc44.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkHotel=self.webpackChunkHotel||[]).push([[350],{8350:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
